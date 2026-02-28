92ea5c22cdd77eae93817321c935f6576206e20b
Mahodi313 <MehdiAli15@outlook.com>
2026-03-01 00:24:28 +0100

    Configuring Httpclient for OllamaService

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using MergeGuard.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddHttpClient<OllamaRiskClient>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["Ollama:BaseUrl"] ?? "http://localhost:11434/api");
+});
 
 var app = builder.Build();
 
